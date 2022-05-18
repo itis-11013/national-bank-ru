e90610915d82a96ff6e7338be0eab71cb50255ec
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-05-19 02:07:41 +0300

    remove useless classes

--- src/main/java/ru/itis/nationalbankru/services/role/RoleService.java
+++
@@ -1,7 +0,0 @@
-package ru.itis.nationalbankru.services.role;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itis.nationalbankru.entity.Role;
-
-public interface RoleService extends JpaRepository<Role, Long> {
-}
