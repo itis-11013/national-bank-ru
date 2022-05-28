481cd0a5742d8a3b209d5662848b636f11f7f873
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-05-29 01:35:55 +0300

    refactored auth controller

--- src/main/java/ru/itis/nationalbankru/controllers/AuthenticationController.java
+++ src/main/java/ru/itis/nationalbankru/controllers/AuthenticationController.java
@@ -3,13 +3,12 @@ package ru.itis.nationalbankru.controllers;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.itis.nationalbankru.dto.user.UserRequestDto;
 import ru.itis.nationalbankru.dto.user.UserResponseDto;
 import ru.itis.nationalbankru.services.user.UserServiceImpl;
@@ -17,11 +16,11 @@ import ru.itis.nationalbankru.services.user.UserServiceImpl;
 @Controller
 @RequestMapping("/auth")
 @Slf4j
-public class AuthController {
+public class AuthenticationController {
 
     private final UserServiceImpl userService;
 
-    public AuthController(UserServiceImpl userService) {
+    public AuthenticationController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
@@ -30,27 +29,21 @@ public class AuthController {
         return "sign_in_page";
     }
 
-    @ApiOperation(value = "Create User")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully Sign", response = UserResponseDto.class),
-            @ApiResponse(code = 401, message = "Authorization required"),
-            @ApiResponse(code = 404, message = "Not found"),
-            @ApiResponse(code = 500, message = "Server error")
-    })
     @GetMapping("/signUp")
     public String getSignUpPage() {
         return "sign_up_page";
     }
 
     @PostMapping("/signUp")
-    public String signUp(UserRequestDto userRequestDto) {
+    public String signUp(UserRequestDto userRequestDto, RedirectAttributes redirectAttributes) {
         try {
             userService.createUser(userRequestDto);
+            return "redirect:/auth/signIn";
         } catch (Exception e) {
             log.error(e.getMessage());
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
             return "redirect:/auth/signUp";
         }
-        return "redirect:/auth/signIn";
     }
 
 }
