db4dff530ce6dcd82fbd3f6df33f098847a94d5a
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-05-29 15:43:47 +0300

    created mapper & repo functions

---
+++ src/main/java/ru/itis/nationalbankru/mappers/OrganizationMapper.java
@@ -0,0 +1,21 @@
+package ru.itis.nationalbankru.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import ru.itis.nationalbankru.dto.organization.OrganizationRequestDto;
+import ru.itis.nationalbankru.dto.organization.OrganizationResponseDto;
+import ru.itis.nationalbankru.entity.Organization;
+
+import java.util.List;
+
+@Mapper
+public interface OrganizationMapper {
+
+    OrganizationResponseDto toDto(Organization organization);
+
+    List<OrganizationResponseDto> toDto(List<Organization> organization);
+
+    Organization fromDto(OrganizationRequestDto organizationResponseDto);
+
+    void updateFromDto(OrganizationRequestDto organizationRequestDto, @MappingTarget Organization organization);
+}
