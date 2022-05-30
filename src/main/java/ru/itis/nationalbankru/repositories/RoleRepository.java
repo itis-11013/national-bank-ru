2cbfc809ab1b30d5c156560e7e29188d36fc2ec2
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-05-30 11:36:10 +0300

    adde contract repo & entityMapper

--- src/main/java/ru/itis/nationalbankru/repositories/RoleRepository.java
+++ src/main/java/ru/itis/nationalbankru/repositories/RoleRepository.java
@@ -3,14 +3,17 @@ package ru.itis.nationalbankru.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.itis.nationalbankru.entity.Role;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
 
     Role findByName(@Param("name") String name);
+
     @Query("select r from User u join u.roles r where u.id = :user_id")
     List<Role> findByUser(@Param("user_id") UUID user_id);
 }
