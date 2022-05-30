90d563279e8fdd3f0b01fec8340df50259717851
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-05-30 08:31:58 +0300

    create product entity

--- src/main/java/ru/itis/nationalbankru/entity/Product.java
+++ src/main/java/ru/itis/nationalbankru/entity/Product.java
@@ -2,13 +2,12 @@ package ru.itis.nationalbankru.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
-@ToString
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -16,5 +15,35 @@ import java.util.UUID;
 @Table(name = "products")
 public class Product extends AbstractEntity {
 
+    @Column(name = "inner_id", nullable = false)
     UUID inner_id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "count", nullable = false)
+    private Integer count;
+
+    @Column(name = "frozen_count", nullable = false)
+    @Builder.Default
+    private Integer frozenCount = 0;
+
+    @ManyToOne
+    @JoinColumn(name = "unit_id")
+    private Unit unit;
+
+    @ManyToOne
+    @JoinColumn(name = "catalog_id")
+    private ProductCatalog catalog;
+
+    @ManyToOne()
+    @JoinColumn(name = "seller_id", nullable = false)
+    private Organization seller;
+
+    @OneToMany(mappedBy = "product")
+    private List<Contract> contracts;
+
 }
