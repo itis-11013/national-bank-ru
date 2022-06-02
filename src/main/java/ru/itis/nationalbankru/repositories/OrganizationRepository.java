525ea348cdc3a5e4f2c7ee952a38be0977f91727
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-06-02 23:30:54 +0300

    update repos

--- src/main/java/ru/itis/nationalbankru/repositories/OrganizationRepository.java
+++ src/main/java/ru/itis/nationalbankru/repositories/OrganizationRepository.java
@@ -1,45 +1,44 @@
 package ru.itis.nationalbankru.repositories;
 
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.itis.nationalbankru.entity.Organization;
-import ru.itis.nationalbankru.entity.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 
-    Page<Organization> findOrganizationsByUser(User user, Pageable pageable);
+    Optional<Organization> findByName(String name);
+
+    Optional<Organization> findOrganizationsByInnerId(UUID uuid);
 
     @Modifying(flushAutomatically = true)
     @Query("UPDATE Organization o SET " +
             " o.balance = o.balance - :contractFee ," +
             " o.frozenBalance = o.frozenBalance + :contractFee " +
             " WHERE o.id = :id ")
-    void freezeContractFees(@Param("id") UUID id, @Param("contractFee") Double contractFee);
+    void freezeContractFees(@Param("id") Long id, @Param("contractFee") Double contractFee);
 
     @Modifying(flushAutomatically = true)
     @Query("UPDATE Organization o SET " +
             " o.balance = o.balance + :contractFee ," +
             " o.frozenBalance = o.frozenBalance - :contractFee " +
             " WHERE o.id = :id ")
-    void refundContractFees(@Param("id") UUID id, @Param("contractFee") Double contractFee);
+    void refundContractFees(@Param("id") Long id, @Param("contractFee") Double contractFee);
 
     @Modifying(flushAutomatically = true)
     @Query("UPDATE Organization o SET o.frozenBalance = o.frozenBalance - :contractFee " +
             " WHERE o.id = :id ")
-    void payContractFees(@Param("id") UUID id, @Param("contractFee") Double contractFee);
+    void payContractFees(@Param("id") Long id, @Param("contractFee") Double contractFee);
 
     @Modifying(flushAutomatically = true)
     @Query("UPDATE Organization o SET " +
             " o.balance = o.balance + :contractFee  WHERE o.id = :id ")
-    void depositContractFees(@Param("id") UUID id, @Param("contractFee") Double contractFee);
+    void depositContractFees(@Param("id") Long id, @Param("contractFee") Double contractFee);
 
 }
