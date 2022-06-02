e47fbe672b6d0f8d5fa00248ef4e4fd907bf9a07
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-06-02 23:29:59 +0300

    add custom exceptions

--- src/main/java/ru/itis/nationalbankru/exceptions/OrganizationNotFoundException.java
+++ src/main/java/ru/itis/nationalbankru/exceptions/OrganizationNotFoundException.java
@@ -1,8 +1,14 @@
 package ru.itis.nationalbankru.exceptions;
 
+import java.util.UUID;
+
 public class OrganizationNotFoundException extends Exception {
 
     protected OrganizationNotFoundException(Long id) {
+        super(String.format("Organization with id %s not found", id));
+    }
+
+    protected OrganizationNotFoundException(UUID id) {
         super(String.format("Organization with id %s not found", id));
     }
 }
