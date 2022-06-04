db23cfb681731ae3fde59def5a87a6e82d52ad5b
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-06-04 22:34:11 +0300

    make cnetral client independent

--- src/main/java/ru/itis/nationalbankru/helpers/OrganizationHelper.java
+++ src/main/java/ru/itis/nationalbankru/helpers/OrganizationHelper.java
@@ -1,13 +1,11 @@
 package ru.itis.nationalbankru.helpers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import ru.itis.nationalbankru.entity.Organization;
 import ru.itis.nationalbankru.repositories.OrganizationRepository;
-
-import java.nio.file.attribute.UserPrincipalNotFoundException;
+import ru.itis.nationalbankru.security.details.UserDetailImpl;
 
 
 @Component
@@ -15,11 +13,14 @@ import java.nio.file.attribute.UserPrincipalNotFoundException;
 public class OrganizationHelper {
     private final OrganizationRepository organizationRepository;
 
-    public Organization getCurrentOrganization() throws UserPrincipalNotFoundException {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String principal = (String) authentication.getPrincipal();
-        return organizationRepository
-                .findByName(principal)
-                .orElseThrow(() -> new UserPrincipalNotFoundException(principal));
+    public Organization getCurrentOrganization() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetailImpl) {
+            username = ((UserDetailImpl) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return organizationRepository.getByName(username);
     }
 }
