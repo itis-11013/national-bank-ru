085e81f57961b8045c42b37b57eeebc65f455979
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-06-05 10:32:15 +0300

    make webclient as bean to not get re initiated

--- src/main/java/ru/itis/nationalbankru/helpers/CentralClient.java
+++ src/main/java/ru/itis/nationalbankru/helpers/CentralClient.java
@@ -2,6 +2,7 @@ package ru.itis.nationalbankru.helpers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -20,11 +21,12 @@ public class CentralClient {
     @Value("${centralBankEndPoint}")
     private String centralBankEndPoint;
 
+    @Bean
     public WebClient getWebClient() {
         if (this.webClient == null) {
             this.webClient = WebClient.create(centralBankEndPoint);
+            log.info(String.format("Using central url : %s ", centralBankEndPoint));
         }
-        log.info(String.format("Using central url : %s ", centralBankEndPoint));
         return webClient;
     }
 }
