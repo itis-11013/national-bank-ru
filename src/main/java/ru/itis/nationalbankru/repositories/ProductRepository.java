a8ccdb68e73e47ec8221eb6d8fb32eaebdb0eb39
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-06-05 17:07:29 +0300

    add functions for repos

--- src/main/java/ru/itis/nationalbankru/repositories/ProductRepository.java
+++ src/main/java/ru/itis/nationalbankru/repositories/ProductRepository.java
@@ -1,6 +1,9 @@
 package ru.itis.nationalbankru.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.itis.nationalbankru.entity.Product;
 
 import java.util.Optional;
@@ -16,4 +19,26 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findProductByInnerId(UUID uuid);
 
     Optional<Product> findProductByNameAndSellerId(String name, Long id);
+
+    @Modifying(flushAutomatically = true)
+    @Query("UPDATE Product p SET " +
+            " p.count = p.count - :contractCount ," +
+            " p.frozenCount = p.frozenCount + :contractCount " +
+            " WHERE p.id = :id ")
+    void freezeContractCount(@Param("id") Long id, @Param("contractCount") Double contractCount);
+
+    @Modifying(flushAutomatically = true)
+    @Query("UPDATE Product p SET " +
+            " p.count = p.count - :contractCount ," +
+            " p.frozenCount = p.frozenCount + :contractCount " +
+            " WHERE p.id = :id ")
+    void unFreezeContractCount(@Param("id") Long id, @Param("contractCount") Double contractCount);
+
+    @Modifying(flushAutomatically = true)
+    @Query("UPDATE Product p SET " +
+            " p.frozenCount = p.frozenCount - :contractCount " +
+            " WHERE p.id = :id ")
+    void payContractCount(@Param("id") Long id, @Param("contractCount") Double contractCount);
+
+
 }
